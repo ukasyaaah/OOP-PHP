c57f5a1b10f32d8c5c48d7c1402d4f93d59402ec
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-04 14:28:55 +0700

    override

---
+++ oophp/inheritance.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'produk.php';
+// Film : Laut Bercerita | Leila S. Chudori - 379 halaman
+
+class Buku extends Produk
+{
+    public $halaman;
+    public $penulis;
+
+    public function __construct(string $kategori, string $judul, string $penulis, int $halaman)
+    {
+        parent::__construct($kategori, $judul);
+        $this->penulis = $penulis;
+        $this->halaman = $halaman;
+    }
+
+    public function getInfo()
+    {
+        echo  parent::getInfo() . " | $this->penulis - $this->halaman halaman" . PHP_EOL;
+    }
+}
+
+class Film extends Produk
+{
+    public string $sutradara;
+    public int $jam;
+
+    public function __construct($kategori, string $judul, string $sutradara, int $jam)
+    {
+        parent::__construct($kategori, $judul);
+        $this->sutradara = $sutradara;
+        $this->jam = $jam;
+    }
+
+    function getInfo()
+    {
+        echo parent::getInfo() . " | $this->sutradara ~ $this->jam jam" . PHP_EOL;
+    }
+}
+
+
+$buku = new Buku('Novel', 'Laut Bercerita', 'Leila S. Chudori', 379);
+echo $buku->getInfo();
+
+$film = new Film('Film', 'Nope', 'Jordan Peele', 2);
+$film->getInfo();
--- oophp/produk.php
+++ oophp/produk.php
@@ -3,38 +3,35 @@
 class Produk
 {
     // Property
-    public $judul = 'Laut Bercerita',
-        $penulis = 'Leila S Chudori';
-
+    public $judul, $kategori;
 
 
     // Constructor : method khusus yg akan dijalankan otomatis ketika instance sebuah class
     // variabel dalam parameter beda dengan properti global di class
     // bisa dikasih nilai default
-    public function __construct(string $judul, string $penulis)
+    public function __construct(string $kategori, string $judul)
     {
         $this->judul = $judul;
-        $this->penulis = $penulis;
+        $this->kategori = $kategori;
     }
-
 
     // Method
-    public function sayHello()
+    public function getInfo()
     {
-
-        // Pake this utk akses property di class yg sama
-        echo "Hello $this->penulis";
+        $inpo = "$this->kategori : $this->judul";
+        return $inpo;
     }
+}
 
-    public function getLabel() {}
+class CetakInfoProduk
+{
+    // Object Type 
+    public function cetak(Produk $produk): string
+    {
+        return $str = "{$produk->judul} | {$produk->kategori}";
+    }
 }
 
 
-$produk = new Produk(
-    judul: 'Pulang',
-    penulis: 'Tere Liye'
-);
 
-var_dump($produk) . PHP_EOL;
 
-$produk->sayHello() . PHP_EOL;
